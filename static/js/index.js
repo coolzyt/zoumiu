79a0687033e718335c6df518cf67f38f601ee77d
赵云涛 <zyuntao@126.com>
2017-06-24 18:00:10 +0800

    no message

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,8 @@
 angular.module('myApp', []).controller('indexCtrl', function($scope) {
 $scope.tools = [
 {id:1, name:"我的IP",url:"myip"},
-{id:2, name:'今日新闻',url:"news.html" }
+{id:2, name:"ocr识别",url:"ocr.html" },
+{id:3, name:"车牌提取",url:"carnum.html" }
 ];
 $scope.jump = function(url){
     window.location= url;
